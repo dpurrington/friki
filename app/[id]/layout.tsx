510711f47bab7acc2aea4f772b9246ccdc77b8c6
Dave Purrington <dave.purrington@gmail.com>
2023-09-13 14:14:52 -0400

    checkpoint

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -4,9 +4,9 @@ export default function ArticleLayout({
   children: React.ReactNode
 }) {
   return (
-    <div>
+    <div className="m-5">
       <section className="flex">
-        <nav className="border-2 w-64 h-[85vh]">Nav content</nav>
+        <nav className="border-2 w-72 h-[85vh]">Nav content</nav>
         <div className="">{children}</div>
       </section>
       <div className="">Temp footer - this is where breadcrumbs will go</div>
--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -4,85 +4,126 @@ import Link from 'next/link'
 import { redirect, notFound } from 'next/navigation'
 import { Article } from '../../src/types'
 
-// TODO: this needs refactoring into components
 // TODO: caching?
 // TODO: concurrency support (timestamps)
 export default async function Page(props: {
   params: { id: string }
   searchParams: any
 }) {
-  const mode = props.searchParams.mode
-  switch (mode) {
-    case 'edit':
-    case 'save':
-      return EditPage(props)
-    default:
-      return ViewPage(props)
-  }
-}
+  // This type casting is ridiculous. Not sure why it's giving a warning in TS
+  // because both expressions appear to be of type Mode
+  const mode: Mode =
+    (Mode[props.searchParams.mode] as unknown as Mode) || Mode.View
+  let article: Article | null = await getArticle(props.params.id)
+  if (article === null) {
+    if (mode === Mode.View) return notFound()
 
-async function ViewPage({ params }: { params: { id: string } }) {
-  const article = await getArticle(params.id)
-  if (article === null) return notFound()
+    article = {
+      id: props.params.id,
+      title: '',
+      content: '',
+    }
+  }
 
   return (
-    <div id="article" className="px-8 py-3">
-      <div className="border-2 flex">
-        <div id="title" className="text-4xl w-[85vw]">
+    <div id="article">
+      <div id="articleHeader" className="border-2 w-[75vw]">
+        <div id="title" className="text-3xl inline-block border-2">
           <h1 className="font-bold">{article.title}</h1>
         </div>
-        <div>
-          <Link href={`/${params.id}?mode=edit`}>Edit</Link>
-        </div>
+        <ControlButtons id={article.id} mode={mode} />
         <hr />
       </div>
-      <div className="prose">
-        <ReactMarkdown>{article.content}</ReactMarkdown>
+      <PageContent article={article} mode={mode} />
+    </div>
+  )
+}
+
+async function ControlButtons(props: { id: string; mode: Mode }) {
+  return (
+    <div className="flex justify-end border-2">
+      <div id="editLink">
+        <Link className="btn" href={`/${props.id}?mode=Edit`}>
+          Edit
+        </Link>
       </div>
     </div>
   )
 }
 
-async function EditPage({ params }: { params: { id: string } }) {
-  // use the article, or a blank one if it does not exist
-  let article = (await getArticle(params.id)) || {
-    id: params.id,
-    title: '',
-    content: '',
-  }
-
+async function PageContent(props: any) {
   async function update(formData: FormData) {
     'use server'
     const formValues: any = Object.fromEntries(formData)
     const updatedArticle: Article = {
-      id: params.id,
+      id: props.article.id,
       ...formValues,
     }
     await saveArticle(updatedArticle)
-    redirect(`/${params.id}`)
+    console.log('redirecting')
+    redirect(`/${props.article.id}`)
+  }
+
+  if (props.mode === Mode.Edit) {
+    return (
+      <div>
+        <form action={update}>
+          <div>
+            <input
+              type="text"
+              defaultValue={props.article.title}
+              name="title"
+            />
+          </div>
+          <textarea
+            defaultValue={props.article.content}
+            name="content"
+            className="textarea textarea-bordered w-[78vw] h-[80vh] p-3"
+          />
+          <EditingControlButtons id={props.article.id} />
+        </form>
+      </div>
+    )
   }
 
   return (
-    <div id="app">
-      <div id="title" className="text-6xl">
-        <h1 className="text-6xl">{article.title}</h1>
-      </div>
-      <form action={update}>
-        <div>
-          <input type="text" defaultValue={article.title} name="title" />
-        </div>
-        <textarea
-          defaultValue={article.content}
-          name="content"
-          className="max-w-xs"
-          rows={80}
-          cols={120}
-        />
-        <div>
-          <input type="submit" value="Save" />
-        </div>
-      </form>
-      <a href={`/${params.id}`}>Cancel Changes</a>
+    <div className="prose">
+      <ReactMarkdown>{props.article.content}</ReactMarkdown>
+    </div>
+  )
+}
+
+enum ButtonType {
+  Edit,
+  Save,
+  Cancel,
+}
+
+enum Mode {
+  View,
+  Edit,
+}
+
+function classForButton(buttonType: ButtonType, mode: Mode) {
+  // this list should be exhaustive
+  switch (buttonType) {
+    case ButtonType.Edit:
+      return mode === Mode.View ? '' : 'hidden'
+    case ButtonType.Save:
+    case ButtonType.Cancel:
+      return mode === Mode.Edit ? '' : 'hidden'
+    default:
+      throw `Unexpected button type encountered: ${buttonType}`
+  }
+}
+
+async function EditingControlButtons(props: { id: string }) {
+  return (
+    <div>
+      <button className="btn m-1">Save</button>
+      <Link className="btn m-1" href={`/${props.id}`}>
+        Cancel
+      </Link>
     </div>
   )
 }
